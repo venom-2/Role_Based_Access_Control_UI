8b25c244fff4d998c085c23090e255aac7a32e6a
Akshat Trivedi <akshattrivedi32@gmail.com>
2024-11-21 15:28:27 +0530

    react setup

---
+++ admin-dashboard/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+
+function App() {
+
+  return (
+    <UserContextProvider>
+      
+    </UserContextProvider>
+  )
+}
+
+export default App
